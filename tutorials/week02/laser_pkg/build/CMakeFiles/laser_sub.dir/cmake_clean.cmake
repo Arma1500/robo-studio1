198f3a03f1f9057487975a8988056732c9078ae2
Arhma <Arhma.Baig@student.uts.edu.au>
2024-08-17 20:46:57 +1000

    tut2 not running

---
+++ tutorials/week02/laser_pkg/build/CMakeFiles/laser_sub.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laser_sub.dir/src/laser_sub.cpp.o"
+  "CMakeFiles/laser_sub.dir/src/laser_sub.cpp.o.d"
+  "laser_sub"
+  "laser_sub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laser_sub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tutorials/week02/laser_pkg/src/laser_sub.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
+
+using std::placeholders::_1;
+
+class LaserSubscriber : public rclcpp::Node
+{
+public:
+  LaserSubscriber()
+  : Node("laser_sub")
+  {
+    auto subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+      "/scan", 10, std::bind(&LaserSubscriber::topic_callback, this, _1));
+  }
+
+private:
+  void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "Ranges between '%f' and '%f'", msg->ranges[0], msg->ranges[0]);
+  }
+
+  //rclcpp::Subscription::SharedPtr subscription_;
+};
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<LaserSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
+
+
